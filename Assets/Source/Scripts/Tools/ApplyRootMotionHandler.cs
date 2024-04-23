b04cdfb6576e54b5184cc6d70978a961489e5fdf
Valanda <dota.doctor@mail.ru>
2024-04-23 21:43:45 +0300

    Some fixes

--- Assets/Source/Scripts/Tools/ApplyRootMotionHandler.cs
+++ Assets/Source/Scripts/Tools/ApplyRootMotionHandler.cs
@@ -1,44 +1,47 @@
 using UnityEngine;
 
-public class ApplyRootMotionHandler : MonoBehaviour
+namespace Source.Scripts.Tools
 {
-    [SerializeField] private CharacterController _characterController;
-    [SerializeField] private Animator _animator;
-    private float _verticalSpeed;
-    [SerializeField] private float _animationRootMotionMultiplayer = 1;
-
-    public void SetAnimationRootMotionMultiplier(float newMultiplier)
+    public class ApplyRootMotionHandler : MonoBehaviour
     {
-        _animationRootMotionMultiplayer = newMultiplier;
-    }
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Animator _animator;
+        private float _verticalSpeed;
+        [SerializeField] private float _animationRootMotionMultiplayer = 1;
 
-    public float GetAnimationMotionMultiplier()
-    {
-        return _animationRootMotionMultiplayer;
-    }
+        public void SetAnimationRootMotionMultiplier(float newMultiplier)
+        {
+            _animationRootMotionMultiplayer = newMultiplier;
+        }
 
-    private void OnAnimatorMove()
-    {
-        Vector3 animatorMove = _animator.deltaPosition * _animationRootMotionMultiplayer;
+        public float GetAnimationMotionMultiplier()
+        {
+            return _animationRootMotionMultiplayer;
+        }
+
+        private void OnAnimatorMove()
+        {
+            Vector3 animatorMove = _animator.deltaPosition * _animationRootMotionMultiplayer;
         
-        Vector3 moveWithGravity = animatorMove + Vector3.up * _verticalSpeed;
-        _characterController.Move(animatorMove);
-        moveWithGravity.x = 0;
-        moveWithGravity.z = 0;
-        if (Mathf.Abs(animatorMove.y) == 0)
-        {
-            _verticalSpeed += Physics.gravity.y * Time.deltaTime;
-            _characterController.Move(moveWithGravity * Time.deltaTime);
-        }
-        else
-        {
-            _verticalSpeed = 0;
-        }
+            Vector3 moveWithGravity = animatorMove + Vector3.up * _verticalSpeed;
+            _characterController.Move(animatorMove);
+            moveWithGravity.x = 0;
+            moveWithGravity.z = 0;
+            if (Mathf.Abs(animatorMove.y) == 0)
+            {
+                _verticalSpeed += Physics.gravity.y * Time.deltaTime;
+                _characterController.Move(moveWithGravity * Time.deltaTime);
+            }
+            else
+            {
+                _verticalSpeed = 0;
+            }
 
 
-        if (_characterController.isGrounded)
-        {
-            _verticalSpeed = 0f;
+            if (_characterController.isGrounded)
+            {
+                _verticalSpeed = 0f;
+            }
         }
     }
 }
